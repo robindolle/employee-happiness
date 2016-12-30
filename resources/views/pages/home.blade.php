8be7dfff738504ad743eeb41e4942ece442de9bf
Robin Dollé <robin.dolle@outlook.be>
2016-12-30 20:08:12 +0100

    Vote buttons

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,6 +8,25 @@
                 <div class="panel-heading">How was your day?</div>
                 <div class="panel-body">
 
+                    {!! Form::open(['url' => '/']) !!}
+                        <div class="form-group" data-toggle="buttons">
+                            <label class="btn btn-success btn-lg">
+                                {!! Form::radio('vote', 'smile') !!}
+                                <i class="fa fa-smile-o" aria-hidden="true"></i>
+                            </label>
+                            <label class="btn btn-warning btn-lg">
+                                {!! Form::radio('vote', 'meh') !!}
+                                <i class="fa fa-meh-o" aria-hidden="true"></i>
+                            </label>
+                            <label class="btn btn-danger btn-lg">
+                                {!! Form::radio('vote', 'frown') !!}
+                                <i class="fa fa-frown-o" aria-hidden="true"></i>
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+                        </div>
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
