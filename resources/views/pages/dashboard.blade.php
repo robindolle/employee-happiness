242b517b54cfaa7a6d15b0f014e5eb253fdd4853
Robin Dollé <robin.dolle@outlook.be>
2016-12-30 21:37:47 +0100

    List all votes on dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,10 +6,24 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Vote</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($votes as $vote)
+                            <tr>
+                                <th>{{ $vote->id }}</th>
+                                <td><i class="fa fa-{{ $vote->vote }}-o" aria-hidden="true"></i> {{ $vote->vote }}</td>
+                                <td>{{ $vote->created_at }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
