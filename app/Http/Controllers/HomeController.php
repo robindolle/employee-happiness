11e46fe58693ff884bf88810a894e5e52df02c86
Robin Dollé <robin.dolle@outlook.be>
2017-01-08 17:15:40 +0100

    Send notification after negative votes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\VoteRequest;
+use App\Notifications\ConsecutiveNegativeVotes;
+use App\User;
 use App\Vote;
 use App\VoteType;
 use Carbon\Carbon;
@@ -30,6 +32,20 @@ class HomeController extends Controller
         $vote->created_at = Carbon::now();
         $vote->save();
 
+        $this->sendNotification();
+
         return redirect('/')->with('flash_message', 'Your vote was successfully submitted.');
+    }
+
+    /**
+     * Send a notification to the manager after 3 consecutive negative votes.
+     */
+    private function sendNotification()
+    {
+        $unique_votes = Vote::latest()->take(3)->get()->unique('vote_type_id')->values()->all();
+        if(count($unique_votes) === 1 && $unique_votes[0]->vote_type_id === 3) {
+            $user = User::first();
+            $user->notify(new ConsecutiveNegativeVotes());
+        }
     }
 }
