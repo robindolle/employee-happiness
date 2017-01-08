6b418708f0137d4796815232fe6700d51b4b9a9c
Robin Dollé <robin.dolle@outlook.be>
2017-01-08 14:24:04 +0100

    Vote statistics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Vote;
-use Illuminate\Http\Request;
+use App\VoteType;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -24,7 +24,40 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $votes = Vote::all();
-        return view('pages.dashboard', compact('votes'));
+
+        $vote_types = VoteType::all();
+
+        $day_votes = $this->getStatistics()->today()->get();
+        $day_votes_total = $day_votes->sum('total');
+
+        $week_votes = $this->getStatistics()->week()->get();
+        $week_votes_total = $week_votes->sum('total');
+
+        $month_votes = $this->getStatistics()->month()->get();
+        $month_votes_total = $month_votes->sum('total');
+
+        $latest_votes = DB::table('votes')
+            ->join('vote_types', 'votes.vote_type_id', '=', 'vote_types.id')
+            ->latest()
+            ->take(5)
+            ->get();
+
+        return view('pages.dashboard', [
+            'vote_types' => $vote_types,
+            'day_votes' => $day_votes,
+            'day_votes_total' => $day_votes_total,
+            'week_votes' => $week_votes,
+            'week_votes_total' => $week_votes_total,
+            'month_votes' => $month_votes,
+            'month_votes_total' => $month_votes_total,
+            'latest_votes' => $latest_votes
+        ]);
+    }
+
+    private function getStatistics()
+    {
+        return Vote::select(DB::raw('MIN(vote_types.name) as name, MIN(vote_types.color) as color, count(*) as total'))
+            ->join('vote_types', 'votes.vote_type_id', '=', 'vote_types.id')
+            ->groupBy('vote_type_id');
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,27 +4,84 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
+                <div class="panel-body">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active">
+                            <a href="#day" aria-controls="day" role="tab" data-toggle="tab">Day</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#week" aria-controls="week" role="tab" data-toggle="tab">Week</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#month" aria-controls="month" role="tab" data-toggle="tab">Month</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="day">
+                            <div class="progress">
+                                @foreach($day_votes as $day_vote)
+                                    <div class="progress-bar progress-bar-{{ $day_vote->color }}" style="width: {{ ($day_vote->total / $day_votes_total) * 100 }}%">
+                                        {{ round(($day_vote->total / $day_votes_total) * 100) }}%
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="week">
+                            <div class="progress">
+                                @foreach($week_votes as $week_vote)
+                                    <div class="progress-bar progress-bar-{{ $week_vote->color }}" style="width: {{ ($week_vote->total / $week_votes_total) * 100 }}%">
+                                        {{ round(($week_vote->total / $week_votes_total) * 100) }}%
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="month">
+                            <div class="progress">
+                                @foreach($month_votes as $month_vote)
+                                    <div class="progress-bar progress-bar-{{ $month_vote->color }}" style="width: {{ ($month_vote->total / $month_votes_total) * 100 }}%">
+                                        {{ round(($month_vote->total / $month_votes_total) * 100) }}%
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="list-inline">
+                        @foreach($vote_types as $vote_type)
+                            <li>
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x text-{{ $vote_type->color }}"></i>
+                                    <i class="fa {{ $vote_type->icon }} fa-stack-1x fa-inverse"></i>
+                                </span>
+                                <span class="text-muted">{{ $vote_type->name }}</span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Latest votes</div>
                 <table class="table">
                     <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Vote</th>
-                            <th>Date</th>
-                        </tr>
+                    <tr>
+                        <th>Vote</th>
+                        <th>Date</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        @foreach($votes as $vote)
-                            <tr>
-                                <th>{{ $vote->id }}</th>
-                                <td><i class="fa fa-{{ $vote->vote }}-o" aria-hidden="true"></i> {{ $vote->vote }}</td>
-                                <td>{{ $vote->created_at }}</td>
-                            </tr>
-                        @endforeach
+                    @foreach($latest_votes as $vote)
+                        <tr>
+                            <td><i class="fa {{ $vote->icon }}" aria-hidden="true"></i> {{ $vote->name }}</td>
+                            <td>{{ $vote->created_at }}</td>
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
+
         </div>
     </div>
 </div>
